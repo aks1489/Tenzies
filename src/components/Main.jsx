2c565312b329192d24dba2bd3ed3f299ff7ca0ea
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-02-22 11:39:18 +0530

    generating random number and pushing/updating to num variable

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,15 @@
 import Die from "./Main_Files/Die";
 
 export default function Main(){
+    function allNewDies() {
+        let num = []
+        for(let i = 0; i< 10 ; i++) {
+            num[i] = Math.floor((Math.random() * 6 ) + 1);
+            // num.push(Math.ceil(Math.random() * 6 ));
+        }
+        return num
+    }
+    console.log(allNewDies())
     return(
         <main>
             <div className="mainContainer">
@@ -8,7 +17,7 @@ export default function Main(){
                 <h5>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h5>
                 <div className="box-container">
                     <Die value="1" />
-                    <Die value="2" />
+                    <Die value="1" />
                     <Die value="3" />
                     <Die value="4" />
                     <Die value="5" />
