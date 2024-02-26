67bbbbae4633dbd35535aa8cb9a93166152408a2
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-02-26 19:21:31 +0530

    fixed: styling
    *header, footer*

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
+// import { githubImg } from "../assets/github.png"
+// import githubImgWhite from "../assets/githubWhite.png"
+import gitwhite from "../assets/gitWhite.png"
+
 export default function Footer(){
     return(
         <footer>
-            This is footer
+            <div className="f-head">
+                <p className="f-head-text">made by - </p>
+                <a className="f-head-link" href="https://github.com/aks1489/">
+                    <div className="f-head-link-container">
+                        <img className="f-head-link-img" src={gitwhite} alt="github" />
+                        <p className="f-head-link-text"> akash</p>
+                    </div>
+                </a>
+            </div>
         </footer>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
         <header>
-            this is header
+            <img src="tenzie.png" alt="tenzies" />
         </header>
     )
 }
