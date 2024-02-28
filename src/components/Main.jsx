988c07916563973b1c6cbdcdceb330166dfe2772
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-02-28 19:48:37 +0530

    update: modal
    ** modal triggering condition (if all the number selected are not same)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react";
 import Die from "./Main_Files/Die";
 import { nanoid } from "nanoid";
 import ReactConfetti from "react-confetti";
+import Wrong from "../assets/wrong.gif"
 
 export default function Main(){
     const [ dies , setDies ] = useState(allNewDies());
     const [ game, setGame ] = useState(false);
+    const [ modal, setModal ] = useState(false)
+    const [ modalDisplay, setModalDispay ] = useState("none")
 
 
     // generating isheld? and key (nanoid) property
@@ -59,7 +62,7 @@ export default function Main(){
         //  {"value" property values are similer to zero(0) index are similer
         //  if all condition are tru then set the 'game' to true -----------< }}
         //
-        //      ------>Code<-------
+        //      ------>Code<------- (incomplete)
         // for( let i = 0; i < 10; i++ ) {
         //     if(dies[i].isHeld && dies[0].value === dies[i].value){
         //         setGame(true)
@@ -73,12 +76,17 @@ export default function Main(){
         const allSameValue = dies.every(die => die.value === dies[0].value)
 
         if (allHeld && !allSameValue){
-            console.log("All numbers are not matched!")
+            // if all number are not matched setting modal status to true
+            setModal(true)
         } else if (allHeld && allSameValue) {
             setGame(true)
         }
 
     }, [dies])
+
+    function closeModal() {
+        setModal(false)
+    }
 
     // mapping over dies for getting 10 dies with prop
     const diesElements = dies.map(die => (
@@ -91,20 +99,35 @@ export default function Main(){
         />)
     )
 
+    // setting modal display property
+    useEffect(() => {
+        setModalDispay(() => modal ? "flex" : "none")
+        console.log(modalDisplay)
+        console.log("triggerd")
+    }, [modal])
+
     return(
-        <main>
-            {game && <ReactConfetti />}
-            <div className="mainContainer">
-                <div className="box-heading">
-                    <h1>Tenzies</h1>
-                    <h5>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h5>
-                </div>
-                <div className="box-container">
-                    {/* {dies.map(die => <Die key={die.key} value={die.value} isHeld={die.isHeld} id={die.key} holdDies={()=>holdDies(die.key)} />)} */}
-                    {diesElements}
-                </div>
-                <button className="roll-dies" onClick={rollDies}>{game ? "New Game" : "Roll"}</button>
+        <>
+            <div className="modal" style={{display : modalDisplay}}>
+                <h2 className="modal-top-text">All number are not matched</h2>
+                <img className="modal-img" src={Wrong} alt="Number not Matched" />
+                <h2 className="modal-bottom-text"> Try to match all the numbers correctly</h2>
+                <button className="modal-close" onClick={closeModal}>Close</button>
             </div>
-        </main>
+            <main style={{ filter: modal ? "blur(5px)" : "none" }}>
+                {game && <ReactConfetti />}
+                <div className="mainContainer">
+                    <div className="box-heading">
+                        <h1>Tenzies</h1>
+                        <h5>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h5>
+                    </div>
+                    <div className="box-container">
+                        {/* {dies.map(die => <Die key={die.key} value={die.value} isHeld={die.isHeld} id={die.key} holdDies={()=>holdDies(die.key)} />)} */}
+                        {diesElements}
+                    </div>
+                    <button className="roll-dies" onClick={rollDies}>{game ? "New Game" : "Roll"}</button>
+                </div>
+            </main>
+        </>
     )
 }
