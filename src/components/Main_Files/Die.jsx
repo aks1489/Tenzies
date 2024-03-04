d00caa750d7c2fb5a9484a6f276d354c46956d82
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-03-04 19:23:56 +0530

    update: dots
    replacing dots instade of using numbers

--- src/components/Main_Files/Die.jsx
+++ src/components/Main_Files/Die.jsx
@@ -1,11 +1,19 @@
+import './die.css'
 export default function Die(props) {
     const styles = {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
     return(
         // <div className={`box ${isHeld ? "green" : ""}`}>
+        // <div className="box" style={styles} onClick={props.holdDies}>
+        //     <h2 className="box-num">{props.value}</h2>
+        // </div>
+
+        // with Dice 
         <div className="box" style={styles} onClick={props.holdDies}>
-            <h2 className="box-num">{props.value}</h2>
+            {[...Array(props.value)].map((index) => {
+                return <span key={index} className='box-dot'></span>
+            })}
         </div>
     )
 }
